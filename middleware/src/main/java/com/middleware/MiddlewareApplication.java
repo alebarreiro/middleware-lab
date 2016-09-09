c9141a20cdeb7637c3d0feecc3b79ef6a116bf5a
Alejandro Barreiro <alebarreiro91@gmail.com>
2016-09-09 20:25:12 -0300

    Agrego log4j2

--- middleware/src/main/java/com/middleware/MiddlewareApplication.java
+++ middleware/src/main/java/com/middleware/MiddlewareApplication.java
@@ -2,11 +2,17 @@ package com.middleware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class MiddlewareApplication {
+	
+	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
 	public static void main(String[] args) {
+		logger.error("Test");
+		
 		SpringApplication.run(MiddlewareApplication.class, args);
 	}
 }
