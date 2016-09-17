2e83db76779a7d20dcea41264f2c5e0519ff1e76
Alejandro Barreiro <alebarreiro91@gmail.com>
2016-09-17 20:17:45 -0300

    Ejemplo jms especifico con activemq producer

---
+++ gs-messaging-jms-client/src/main/java/msg/Item.java
@@ -0,0 +1,18 @@
+package msg;
+
+public class Item {
+
+    private String nombre;
+
+    public Item(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
--- gs-messaging-jms-client/src/main/java/msg/Receiver.java
+++ gs-messaging-jms-client/src/main/java/msg/Receiver.java
@@ -5,8 +5,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
-    @JmsListener(destination = "mailbox", containerFactory = "myFactory")
-    public void receiveMessage(Email email) {
-        System.out.println("Received <" + email + ">");
-    }
+
 }
