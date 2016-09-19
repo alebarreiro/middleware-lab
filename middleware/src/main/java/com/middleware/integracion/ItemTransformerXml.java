e617827e25e67b4956321f4b6a11be9427ccb286
Anthony Cabrera <cabreratony4@gmail.com>
2016-09-19 17:20:43 -0300

    Se agrega transformador de Item a XML

---
+++ middleware/src/main/java/com/middleware/integracion/ItemTransformerXml.java
@@ -0,0 +1,46 @@
+package com.middleware.integracion;
+
+import com.middleware.clases.Item;
+import com.middleware.clases.ItemMessage;
+import com.middleware.clases.OrderMessage;
+import com.middleware.clases.ProcessableItem;
+import com.middleware.util.JaxbContext;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+
+public class ItemTransformerXml {
+
+    public String transform(ItemMessage itemMessage) {
+        OrderMessage orderMessage = new OrderMessage();
+        String itemXml = null;
+
+        try {
+            itemXml = jaxbUnmarshal(itemMessage);
+        } catch (JAXBException e) {
+            System.out.println("Error al parsear el item a xml:" + e.getMessage());
+        }
+
+        return itemXml;
+    }
+
+    private String jaxbUnmarshal(ItemMessage itemMessage) throws JAXBException {
+        JAXBContext jaxbContext = JaxbContext.newInstance();
+
+        Marshaller jaxbUnmarshaller = jaxbContext.createMarshaller();
+
+        StringWriter stringWriter = new StringWriter();
+
+        ProcessableItem processableItem = new ProcessableItem(
+                itemMessage.getItem(),
+                itemMessage.getIdCliente(),
+                itemMessage.getIdOrden()
+        );
+
+        jaxbUnmarshaller.marshal(processableItem, stringWriter);
+
+        return stringWriter.toString();
+    }
+}
