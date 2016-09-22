f44906af26723e6cb6036d2ad033604c76bff0ce
Alejandro Barreiro <alebarreiro91@gmail.com>
2016-09-22 19:51:18 -0300

    Atributos orden

--- gs-messaging-jms-client/src/main/java/msg/dto/Order.java
+++ gs-messaging-jms-client/src/main/java/msg/dto/Order.java
@@ -1,14 +1,22 @@
 package msg.dto;
 
 import java.util.List;
+import java.util.Date;
 
-/**
- * Created by alejandrobarreiro on 21/9/16.
- */
 public class Order {
 
-    private List<Item> Items;
+    //Orden
+    private long numero;
+    private Date fecha;
+    private long clientId;
+    private String formaPago; //E = Efectivo, C = Cheque, D = Debito, X = Crédito
 
+    //Facturacion
+    private int moneda; //858 = pesos, 840 = dolares
+    private int cuotas; //1 efectivo, cheque o debito, N para credito
+
+    //Items*
+    private List<Item> Items;
 
     public void Order () {}
 
