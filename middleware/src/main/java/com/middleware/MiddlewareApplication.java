0b817e1da527e21bcee2de8a80b3ae2bc4c7a873
Anthony Cabrera <cabreratony4@gmail.com>
2016-09-24 21:02:29 -0300

    Se agrega aggregator de paquete de items, previo a la comunicacion con el ws strawberry

--- middleware/src/main/java/com/middleware/MiddlewareApplication.java
+++ middleware/src/main/java/com/middleware/MiddlewareApplication.java
@@ -1,20 +1,13 @@
 package com.middleware;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.ApplicationContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class MiddlewareApplication {
-	
-	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
 	public static void main(String[] args) {
-		logger.error("Test");
-
 		ApplicationContext context =
 				new ClassPathXmlApplicationContext(
 						"/META-INF/spring/integration/si-config.xml",
