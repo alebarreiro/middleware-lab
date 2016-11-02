7ba864a9c488243932c035323cdb059e02de8706
Alejandro Barreiro <alebarreiro91@gmail.com>
2016-11-02 11:20:09 -0300

    Remove unused imports

--- lab-2/ticketinco-soap/src/main/java/ticketinco/datatype/DataPagoLocal.java
+++ lab-2/ticketinco-soap/src/main/java/ticketinco/datatype/DataPagoLocal.java
@@ -1,8 +1,6 @@
 package ticketinco.datatype;
 
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
